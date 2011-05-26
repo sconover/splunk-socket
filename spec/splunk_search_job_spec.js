5373329fef2b9d087baddf4b876719d5c3a0409e
Steve Conover <steve@squareup.com>
2011-05-25 21:54:41 -0700

    extract splunkhttp

--- spec/splunk_search_job_spec.js
+++ spec/splunk_search_job_spec.js
@@ -1,59 +1,99 @@
 var fakeHttpFactory = require('./fake_http').fakeHttpFactory
-var SplunkSearchJob = require('../splunk_search').SplunkSearchJob
+var splunk = require('../splunk_search')
+var SplunkHttp = splunk.SplunkHttp
+var SplunkSearchJob = splunk.SplunkSearchJob
 
 describe('splunk search job', function(){
   beforeEach(function(){
     this.fakeHttp = fakeHttpFactory()
   })
   
-  it('posts a search job', function(){
-    var http = this.fakeHttp,
-        httpRequestSpy = spyOn(http, 'request').andCallFake(function() {return http}),
-        httpHeaderSpy = spyOn(http, 'setHeader').andCallThrough()
+  
+  describe('a new search job', function(){
+    it('creates a job', function(){
+      var http = this.fakeHttp,
+          httpRequestSpy = spyOn(http, 'request').andCallFake(function() {return http}),
+          httpHeaderSpy = spyOn(http, 'setHeader').andCallThrough()
     
-    var job = new SplunkSearchJob(http, {
-                user: "bob",
-                password: "pass",
-                host: "splunk.example.com",
-                port: 8089
-              })
-    job.create(function(){})
+      var job = new SplunkSearchJob(new SplunkHttp(http, {
+                                      user: "bob",
+                                      password: "pass",
+                                      host: "splunk.example.com",
+                                      port: 8089
+                                    }), 
+                                    {search:"foo"})
+      job.create(function(){})
 
-    expect(httpRequestSpy.mostRecentCall.args[0]).toEqual({
-      path : '/services/search/jobs',
-      host: "splunk.example.com",
-      port: 8089,
-      method: 'POST'
-    })
+      expect(httpRequestSpy.mostRecentCall.args[0]).toEqual({
+        path : '/services/search/jobs',
+        host: "splunk.example.com",
+        port: 8089,
+        method: 'POST'
+      })
     
-    expect(httpHeaderSpy.argsForCall[0]).toEqual(
-      ['Authorization', 'Basic ' + new Buffer('bob:pass').toString('base64')]
-    )
+      expect(httpHeaderSpy.argsForCall[0]).toEqual(
+        ['Authorization', 'Basic ' + new Buffer('bob:pass').toString('base64')]
+      )
+    })
   })
+  
+  describe('fetching results for an existing search job', function(){
     
-  it('fetches json results for a job', function(){
-    var http = this.fakeHttp,
-        httpRequestSpy = spyOn(http, 'request').andCallFake(function() {return http}),
-        httpHeaderSpy = spyOn(http, 'setHeader').andCallThrough()
+    it('makes a call to get results for the job', function(){
+      var http = this.fakeHttp,
+          httpRequestSpy = spyOn(http, 'request').andCallFake(function() {return http}),
+          httpHeaderSpy = spyOn(http, 'setHeader').andCallThrough()
     
-    var job = new SplunkSearchJob(http, {
-                user: "bob",
-                password: "pass",
-                host: "splunk.example.com",
-                port: 8089
-              })
-    job._jobId = "1234.567"    
-    job.fetchJsonResultsForJob(function(){}, function(){})
+      var job = new SplunkSearchJob(new SplunkHttp(http, {
+                                      user: "bob",
+                                      password: "pass",
+                                      host: "splunk.example.com",
+                                      port: 8089
+                                    }),
+                                    {search:"foo"})
+      job._jobId = "1234.567"    
+      job.fetchJsonResultsForJob(function(){}, function(){})
     
-    expect(httpRequestSpy.mostRecentCall.args[0]).toEqual({
-      path : '/services/search/jobs/1234.567/results?output_mode=json&offset=0',
-      host: "splunk.example.com",
-      port: 8089,
-      method: 'GET'
+      expect(httpRequestSpy.mostRecentCall.args[0]).toEqual({
+        path : '/services/search/jobs/1234.567/results?output_mode=json&offset=0',
+        host: "splunk.example.com",
+        port: 8089,
+        method: 'GET'
+      })
+    
+      expect(httpHeaderSpy.argsForCall[0]).toEqual(
+        ['Authorization', 'Basic ' + new Buffer('bob:pass').toString('base64')])
     })
     
-    expect(httpHeaderSpy.argsForCall[0]).toEqual(
-      ['Authorization', 'Basic ' + new Buffer('bob:pass').toString('base64')])
+    // it('makes a call to get results for the job', function(){
+    //   
+    // })
+    
+    it('more results - makes a call with the incremented offset', function(){
+      // var http = this.fakeHttp,
+      //     httpRequestSpy = spyOn(http, 'request').andCallFake(function() {return http}),
+      //     httpHeaderSpy = spyOn(http, 'setHeader').andCallThrough()
+      //     
+      // var job = new SplunkSearchJob(new SplunkHttp(http), {
+      //             user: "bob",
+      //             password: "pass",
+      //             host: "splunk.example.com",
+      //             port: 8089
+      //           })
+      // job._jobId = "1234.567"    
+      // job.fetchJsonResultsForJob(function(){}, function(){})
+      //     
+      // expect(httpRequestSpy.mostRecentCall.args[0]).toEqual({
+      //   path : '/services/search/jobs/1234.567/results?output_mode=json&offset=0',
+      //   host: "splunk.example.com",
+      //   port: 8089,
+      //   method: 'GET'
+      // })
+      //     
+      // expect(httpHeaderSpy.argsForCall[0]).toEqual(
+      //   ['Authorization', 'Basic ' + new Buffer('bob:pass').toString('base64')])
+    })
+    
   })
   
   
--- splunk_search.js
+++ splunk_search.js
@@ -5,96 +5,116 @@ function extend(toExtend, other) {
   return toExtend
 }
 
-function simpleRequest(http, options, requestConfigCallback, responseBodyCallback) {
-  var responseBody = ''
-  var request = http.request({
-    host: options.host, 
-    port: options.port, 
-    path: options.path,
-    method: options.method
-  }, function(response){
-    response.on('data', function(chunk){ responseBody = responseBody + chunk })
-    response.on('end',       function(){ responseBodyCallback(responseBody) })
-  })
-  request.setHeader('Authorization', 
-                    'Basic ' + new Buffer(options.user + ':' + options.password).toString('base64'))
-  requestConfigCallback(request)
-  request.end()  
+var SplunkHttp = exports.SplunkHttp = function(http, options) {
+  function simpleRequest(http, options, requestConfigCallback, responseBodyCallback) {
+    var responseBody = ''
+    var request = http.request({
+      host: options.host, 
+      port: options.port, 
+      path: options.path,
+      method: options.method
+    }, function(response){
+      response.on('data', function(chunk){ responseBody = responseBody + chunk })
+      response.on('end',       function(){ responseBodyCallback(responseBody) })
+    })
+    request.setHeader('Authorization', 
+                      'Basic ' + new Buffer(options.user + ':' + options.password).toString('base64'))
+    requestConfigCallback(request)
+    request.end()  
+  }
+
+  this.get = function(path, responseBodyCallback) {
+    simpleRequest(http, 
+                  extend({path:path, method:'GET'}, options), 
+                  function(){}, 
+                  responseBodyCallback)
+  }
+  
+  this.post = function(path, params, responseBodyCallback) {
+    simpleRequest(http, 
+                  extend({path:path, method:'POST'}, options), 
+                  function(request){ 
+                    var postBody = QueryString.stringify(params)
+                    request.setHeader('Content-Length', "" + postBody.length)
+                    request.write(postBody) 
+                  }, 
+                  responseBodyCallback)  
+  }
 }
 
-function simplePOST(http, options, responseBodyCallback) {
-  options.method = 'POST'
-  simpleRequest(http, 
-                options, 
-                function(request){ 
-                  var postBody = QueryString.stringify(options.params)
-                  request.setHeader('Content-Length', "" + postBody.length)
-                  request.write(postBody) 
-                }, 
-                responseBodyCallback)  
-}
-
-function simpleGET(http, options, responseBodyCallback) {
-  options.method = 'GET'
-  simpleRequest(http, 
-                options, 
-                function(){}, 
-                responseBodyCallback)
-}
-
-exports.SplunkSearchJob = function(http, requestInfo) {
-  var basicSplunkHttpOptions = {
-        user: requestInfo.user, 
-        password: requestInfo.password, 
-        host: requestInfo.host, 
-        port: requestInfo.port
-      }
+exports.SplunkSearchJob = function(splunkHttp, requestInfo) {
   this._jobId = null
   
   this.create = function(gotJobIdCallback) {
     var self = this
-    simplePOST(
-      http,
-      extend({
-        path: '/services/search/jobs',
-        params:{
-          search: 'search ' + requestInfo.search, 
-          max_count: "" + (requestInfo.max_count || 100),
-          required_field_list:"*" //forces splunk to output k-v pair style
-                                   //see http://splunk-base.splunk.com/answers/24551/structured-fields-and-values-in-json-api-results
-        } 
-      }, basicSplunkHttpOptions),
-      function(responseBody){
+    
+    splunkHttp.post(
+      '/services/search/jobs', 
+      { search: 'search ' + requestInfo.search, 
+        max_count: "" + (requestInfo.max_count || 100),
+        required_field_list:"*" //forces splunk to output k-v pair style
+                                 //see http://splunk-base.splunk.com/answers/24551/structured-fields-and-values-in-json-api-results
+      },
+      function(responseBody) {
         var getJobIdRegexp = /\<sid\>(.*?)\<\/sid\>/,
             match = getJobIdRegexp.exec(responseBody)
         
         self._jobId = match[1]
-        gotJobIdCallback(self._jobId)
+        gotJobIdCallback(self._jobId)      
       }
     )
+    
+    // simplePOST(
+    //   http,
+    //   extend({
+    //     path: '/services/search/jobs',
+    //     params:{
+    //       search: 'search ' + requestInfo.search, 
+    //       max_count: "" + (requestInfo.max_count || 100),
+    //       required_field_list:"*" //forces splunk to output k-v pair style
+    //                                //see http://splunk-base.splunk.com/answers/24551/structured-fields-and-values-in-json-api-results
+    //     } 
+    //   }, basicSplunkHttpOptions),
+    //   function(responseBody){
+    //     var getJobIdRegexp = /\<sid\>(.*?)\<\/sid\>/,
+    //         match = getJobIdRegexp.exec(responseBody)
+    //     
+    //     self._jobId = match[1]
+    //     gotJobIdCallback(self._jobId)
+    //   }
+    // )
   }
   
   function checkWhetherWeHaveAllResults(jobId, resultsOffset, callback) {
-    simpleGET(
-      http, 
-      extend({path: '/services/search/jobs/' + jobId}, basicSplunkHttpOptions),
+    splunkHttp.get(
+      '/services/search/jobs/' + jobId, 
       function(responseBody) { 
         var done = responseBody.indexOf('"isDone">1</') >= 0
         var getResultCountRegexp = /name="resultCount">(.*?)<\/s:key>/,
             match = getResultCountRegexp.exec(responseBody),
             resultCount = parseInt(match[1])
         callback(done && resultsOffset >= resultCount) 
-      }
-    )    
+      })
+    // simpleGET(
+    //   http, 
+    //   extend({path: '/services/search/jobs/' + jobId}, basicSplunkHttpOptions),
+    //   function(responseBody) { 
+    //     var done = responseBody.indexOf('"isDone">1</') >= 0
+    //     var getResultCountRegexp = /name="resultCount">(.*?)<\/s:key>/,
+    //         match = getResultCountRegexp.exec(responseBody),
+    //         resultCount = parseInt(match[1])
+    //     callback(done && resultsOffset >= resultCount) 
+    //   }
+    // )    
   }
   
   this.fetchJsonResultsForJob = function(nextResultsCallback, doneCallback, resultsOffset) {
     resultsOffset = resultsOffset || 0
     var self = this
-    simpleGET(
-      http, 
-      extend({path: '/services/search/jobs/' + self._jobId + '/results?output_mode=json&offset=' + resultsOffset},
-             basicSplunkHttpOptions),
+
+
+    splunkHttp.get(
+      '/services/search/jobs/' + self._jobId + '/results?output_mode=json&offset=' + resultsOffset, 
       function(responseBody) {
         if (responseBody) {
           var results = JSON.parse(responseBody)
@@ -115,6 +135,31 @@ exports.SplunkSearchJob = function(http, requestInfo) {
         }
 
       })
+
+    // simpleGET(
+    //   http, 
+    //   extend({path: '/services/search/jobs/' + self._jobId + '/results?output_mode=json&offset=' + resultsOffset},
+    //          basicSplunkHttpOptions),
+    //   function(responseBody) {
+    //     if (responseBody) {
+    //       var results = JSON.parse(responseBody)
+    //       nextResultsCallback(results)
+    //       var adjustedResultsOffset = resultsOffset + results.length
+    //       checkWhetherWeHaveAllResults(self._jobId, adjustedResultsOffset, function(done){
+    //         if (done) {
+    //           doneCallback()
+    //         } else {
+    //           self.fetchJsonResultsForJob(nextResultsCallback, doneCallback, adjustedResultsOffset)
+    //         }
+    //       })        
+    //     } else {
+    //       throw new Error("no results. test-drive changes to handle this.")
+    //       // setTimeout(function() {
+    //       //   again(nextResultsCallback, doneCallback, adjustedResultsOffset)          
+    //       // }, 500)
+    //     }
+    // 
+    //   })
   }
   
   
@@ -123,6 +168,15 @@ exports.SplunkSearchJob = function(http, requestInfo) {
 exports.SplunkSearch = function(requestInfo, configCallback) {
   var http = requestInfo.http || require("https")
   delete requestInfo.http
+  
+  var splunkHttp = 
+    new SplunkHttp(http, {
+      user: requestInfo.user, 
+      password: requestInfo.password, 
+      host: requestInfo.host, 
+      port: requestInfo.port
+    })
+  
   
   var nextResultsCallback = function(){}
   this.onNextResults = function(callback){nextResultsCallback = callback}
@@ -135,7 +189,7 @@ exports.SplunkSearch = function(requestInfo, configCallback) {
   this.run = function(){
     var resultsOffset = 0
     
-    var job = new exports.SplunkSearchJob(http, requestInfo)    
+    var job = new exports.SplunkSearchJob(splunkHttp, requestInfo)    
     job.create(function(jobId){ job.fetchJsonResultsForJob(nextResultsCallback, doneCallback) })
                 // 
                 // 
