cc76bc772fd94929aa69da8fec12e9da4e377622
Steve Conover <steve@squareup.com>
2011-05-25 22:08:27 -0700

    redoing this test using spies on splunkhttp

--- spec/splunk_search_job_spec.js
+++ spec/splunk_search_job_spec.js
@@ -5,97 +5,86 @@ var SplunkSearchJob = splunk.SplunkSearchJob
 
 describe('splunk search job', function(){
   beforeEach(function(){
-    this.fakeHttp = fakeHttpFactory()
+    this.splunkHttp = new SplunkHttp(fakeHttpFactory(), {
+                            user: "bob",
+                            password: "pass",
+                            host: "splunk.example.com",
+                            port: 8089
+                          })
   })
   
   
   describe('a new search job', function(){
     it('creates a job', function(){
-      var http = this.fakeHttp,
-          httpRequestSpy = spyOn(http, 'request').andCallFake(function() {return http}),
-          httpHeaderSpy = spyOn(http, 'setHeader').andCallThrough()
+      var splunkHttpSpy = spyOn(this.splunkHttp, 'post').andCallFake(function() {})
     
-      var job = new SplunkSearchJob(new SplunkHttp(http, {
-                                      user: "bob",
-                                      password: "pass",
-                                      host: "splunk.example.com",
-                                      port: 8089
-                                    }), 
-                                    {search:"foo"})
+      var job = new SplunkSearchJob(this.splunkHttp, {search:"source=foo | head 1"})
       job.create(function(){})
 
-      expect(httpRequestSpy.mostRecentCall.args[0]).toEqual({
-        path : '/services/search/jobs',
-        host: "splunk.example.com",
-        port: 8089,
-        method: 'POST'
-      })
-    
-      expect(httpHeaderSpy.argsForCall[0]).toEqual(
-        ['Authorization', 'Basic ' + new Buffer('bob:pass').toString('base64')]
-      )
+      expect(splunkHttpSpy.mostRecentCall.args[0]).toEqual('/services/search/jobs')
+      expect(splunkHttpSpy.mostRecentCall.args[1].search).toEqual('search source=foo | head 1')
     })
   })
   
-  describe('fetching results for an existing search job', function(){
-    
-    it('makes a call to get results for the job', function(){
-      var http = this.fakeHttp,
-          httpRequestSpy = spyOn(http, 'request').andCallFake(function() {return http}),
-          httpHeaderSpy = spyOn(http, 'setHeader').andCallThrough()
-    
-      var job = new SplunkSearchJob(new SplunkHttp(http, {
-                                      user: "bob",
-                                      password: "pass",
-                                      host: "splunk.example.com",
-                                      port: 8089
-                                    }),
-                                    {search:"foo"})
-      job._jobId = "1234.567"    
-      job.fetchJsonResultsForJob(function(){}, function(){})
-    
-      expect(httpRequestSpy.mostRecentCall.args[0]).toEqual({
-        path : '/services/search/jobs/1234.567/results?output_mode=json&offset=0',
-        host: "splunk.example.com",
-        port: 8089,
-        method: 'GET'
-      })
-    
-      expect(httpHeaderSpy.argsForCall[0]).toEqual(
-        ['Authorization', 'Basic ' + new Buffer('bob:pass').toString('base64')])
-    })
-    
-    // it('makes a call to get results for the job', function(){
-    //   
-    // })
-    
-    it('more results - makes a call with the incremented offset', function(){
-      // var http = this.fakeHttp,
-      //     httpRequestSpy = spyOn(http, 'request').andCallFake(function() {return http}),
-      //     httpHeaderSpy = spyOn(http, 'setHeader').andCallThrough()
-      //     
-      // var job = new SplunkSearchJob(new SplunkHttp(http), {
-      //             user: "bob",
-      //             password: "pass",
-      //             host: "splunk.example.com",
-      //             port: 8089
-      //           })
-      // job._jobId = "1234.567"    
-      // job.fetchJsonResultsForJob(function(){}, function(){})
-      //     
-      // expect(httpRequestSpy.mostRecentCall.args[0]).toEqual({
-      //   path : '/services/search/jobs/1234.567/results?output_mode=json&offset=0',
-      //   host: "splunk.example.com",
-      //   port: 8089,
-      //   method: 'GET'
-      // })
-      //     
-      // expect(httpHeaderSpy.argsForCall[0]).toEqual(
-      //   ['Authorization', 'Basic ' + new Buffer('bob:pass').toString('base64')])
-    })
-    
-  })
-  
-  
+  // describe('fetching results for an existing search job', function(){
+  //   
+  //   it('makes a call to get results for the job', function(){
+  //     var http = this.fakeHttp,
+  //         httpRequestSpy = spyOn(http, 'request').andCallFake(function() {return http}),
+  //         httpHeaderSpy = spyOn(http, 'setHeader').andCallThrough()
+  //   
+  //     var job = new SplunkSearchJob(new SplunkHttp(http, {
+  //                                     user: "bob",
+  //                                     password: "pass",
+  //                                     host: "splunk.example.com",
+  //                                     port: 8089
+  //                                   }),
+  //                                   {search:"foo"})
+  //     job._jobId = "1234.567"    
+  //     job.fetchJsonResultsForJob(function(){}, function(){})
+  //   
+  //     expect(httpRequestSpy.mostRecentCall.args[0]).toEqual({
+  //       path : '/services/search/jobs/1234.567/results?output_mode=json&offset=0',
+  //       host: "splunk.example.com",
+  //       port: 8089,
+  //       method: 'GET'
+  //     })
+  //   
+  //     expect(httpHeaderSpy.argsForCall[0]).toEqual(
+  //       ['Authorization', 'Basic ' + new Buffer('bob:pass').toString('base64')])
+  //   })
+  //   
+  //   // it('makes a call to get results for the job', function(){
+  //   //   
+  //   // })
+  //   
+  //   it('more results - makes a call with the incremented offset', function(){
+  //     // var http = this.fakeHttp,
+  //     //     httpRequestSpy = spyOn(http, 'request').andCallFake(function() {return http}),
+  //     //     httpHeaderSpy = spyOn(http, 'setHeader').andCallThrough()
+  //     //     
+  //     // var job = new SplunkSearchJob(new SplunkHttp(http), {
+  //     //             user: "bob",
+  //     //             password: "pass",
+  //     //             host: "splunk.example.com",
+  //     //             port: 8089
+  //     //           })
+  //     // job._jobId = "1234.567"    
+  //     // job.fetchJsonResultsForJob(function(){}, function(){})
+  //     //     
+  //     // expect(httpRequestSpy.mostRecentCall.args[0]).toEqual({
+  //     //   path : '/services/search/jobs/1234.567/results?output_mode=json&offset=0',
+  //     //   host: "splunk.example.com",
+  //     //   port: 8089,
+  //     //   method: 'GET'
+  //     // })
+  //     //     
+  //     // expect(httpHeaderSpy.argsForCall[0]).toEqual(
+  //     //   ['Authorization', 'Basic ' + new Buffer('bob:pass').toString('base64')])
+  //   })
+  //   
+  // })
+  // 
+  // 
   
 })
