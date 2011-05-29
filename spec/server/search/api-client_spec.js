036840245b3c60a567d8181221ab60ca1ac6bf38
Steve Conover <steve@squareup.com>
2011-05-29 10:45:40 -0700

    renaming

---
+++ spec/server/search/api-client_spec.js
@@ -0,0 +1,69 @@
+var fakeHttpFactory = require('./fake_http').fakeHttpFactory
+var ApiClient = require('../../../lib/splunk-socket/search/api-client')
+var QueryString = require("querystring")
+
+describe('splunk http', function(){
+  beforeEach(function(){
+    this.fakeHttp = fakeHttpFactory()
+  })
+  
+  
+  it('executes a proper GET', function(){
+    var http = this.fakeHttp,
+        httpRequestSpy = spyOn(http, 'request').andCallFake(function() {return http}),
+        httpHeaderSpy = spyOn(http, 'setHeader').andCallThrough()
+  
+    var apiClient = new ApiClient(http, {
+                                    user: "bob",
+                                    password: "pass",
+                                    host: "splunk.example.com",
+                                    port: 8089
+                                  })
+    apiClient.get('/foo', function(){})
+
+    expect(httpRequestSpy.mostRecentCall.args[0]).toEqual({
+      path : '/foo',
+      host: "splunk.example.com",
+      port: 8089,
+      method: 'GET'
+    })
+  
+    expect(httpHeaderSpy.argsForCall[0]).toEqual(
+      ['Authorization', 'Basic ' + new Buffer('bob:pass').toString('base64')]
+    )
+  })
+  
+  it('executes a proper POST', function(){
+    var http = this.fakeHttp,
+        httpRequestSpy = spyOn(http, 'request').andCallFake(function() {return http}),
+        httpWriteSpy = spyOn(http, 'write').andCallThrough(),
+        httpHeaderSpy = spyOn(http, 'setHeader').andCallThrough()
+  
+    var apiClient = new ApiClient(http, {
+                                    user: "bob",
+                                    password: "pass",
+                                    host: "splunk.example.com",
+                                    port: 8089
+                                  })
+    apiClient.post('/foo', {a:"1", b:"2"}, function(){})
+
+    expect(httpRequestSpy.mostRecentCall.args[0]).toEqual({
+      path : '/foo',
+      host: "splunk.example.com",
+      port: 8089,
+      method: 'POST'
+    })
+  
+    expect(httpWriteSpy.mostRecentCall.args[0]).toEqual(QueryString.stringify({a:"1", b:"2"}))
+  
+    expect(httpHeaderSpy.argsForCall[0]).toEqual(
+      ['Authorization', 'Basic ' + new Buffer('bob:pass').toString('base64')]
+    )
+    expect(httpHeaderSpy.argsForCall[1]).toEqual(
+      ['Content-Length', '' + QueryString.stringify({a:"1", b:"2"}).length]
+    )
+
+  })
+  
+  
+})
--- spec/server/search/splunk_search_job_spec.js
+++ spec/server/search/splunk_search_job_spec.js
@@ -1,10 +1,10 @@
 var fakeHttpFactory = require('./fake_http').fakeHttpFactory
-var SplunkHttp = require('../../../lib/splunk-socket/search/splunk-http')
+var ApiClient = require('../../../lib/splunk-socket/search/api-client')
 var SplunkSearchJob = require('../../../lib/splunk-socket/search/job')
 
 describe('splunk search job', function(){
   beforeEach(function(){
-    this.splunkHttp = new SplunkHttp(fakeHttpFactory(), {
+    this.apiClient = new ApiClient(fakeHttpFactory(), {
                             user: "bob",
                             password: "pass",
                             host: "splunk.example.com",
@@ -15,13 +15,13 @@ describe('splunk search job', function(){
   
   describe('a new search job', function(){
     it('creates a job', function(){
-      var splunkHttpSpy = spyOn(this.splunkHttp, 'post').andCallFake(function() {})
+      var apiClientSpy = spyOn(this.apiClient, 'post').andCallFake(function() {})
     
-      var job = new SplunkSearchJob(this.splunkHttp, {search:"source=foo | head 1"})
+      var job = new SplunkSearchJob(this.apiClient, {search:"source=foo | head 1"})
       job.create(function(){})
 
-      expect(splunkHttpSpy.mostRecentCall.args[0]).toEqual('/services/search/jobs')
-      expect(splunkHttpSpy.mostRecentCall.args[1].search).toEqual('search source=foo | head 1')
+      expect(apiClientSpy.mostRecentCall.args[0]).toEqual('/services/search/jobs')
+      expect(apiClientSpy.mostRecentCall.args[1].search).toEqual('search source=foo | head 1')
     })
   })
   
@@ -29,35 +29,35 @@ describe('splunk search job', function(){
     
     describe('requesting results', function(){
       it('makes a call to get results for the job', function(){
-        var splunkHttpSpy = spyOn(this.splunkHttp, 'get').andCallFake(function() {})
+        var apiClientSpy = spyOn(this.apiClient, 'get').andCallFake(function() {})
     
-        var job = new SplunkSearchJob(this.splunkHttp, {search:"foo"})
+        var job = new SplunkSearchJob(this.apiClient, {search:"foo"})
         job._jobId = "1234.567"    
         job.fetchJsonResultsForJob(function(){}, function(){})
 
-        expect(splunkHttpSpy.mostRecentCall.args[0]).toEqual('/services/search/jobs/1234.567/results?output_mode=json&offset=0')
+        expect(apiClientSpy.mostRecentCall.args[0]).toEqual('/services/search/jobs/1234.567/results?output_mode=json&offset=0')
       })
     
       it('makes a call with the incremented offset to get more results', function(){
-        var splunkHttpSpy = spyOn(this.splunkHttp, 'get').andCallFake(function() {})
+        var apiClientSpy = spyOn(this.apiClient, 'get').andCallFake(function() {})
     
-        var job = new SplunkSearchJob(this.splunkHttp, {search:"foo"})
+        var job = new SplunkSearchJob(this.apiClient, {search:"foo"})
         job._jobId = "1234.567"    
         job.fetchJsonResultsForJob(function(){}, function(){}, 7)
 
-        expect(splunkHttpSpy.mostRecentCall.args[0]).toEqual('/services/search/jobs/1234.567/results?output_mode=json&offset=7')
+        expect(apiClientSpy.mostRecentCall.args[0]).toEqual('/services/search/jobs/1234.567/results?output_mode=json&offset=7')
       })
     })
     
     
     describe('getting results', function(){
       it("returns next results, calls back. not all results have arrived yet.", function(){
-        var job = new SplunkSearchJob(this.splunkHttp, {search:"foo"})
+        var job = new SplunkSearchJob(this.apiClient, {search:"foo"})
         job.checkWhetherWeHaveAllResults = function(offsetNotImportant, allResultsCallback){
           allResultsCallback(false)
         }
 
-        var splunkHttpSpy = spyOn(this.splunkHttp, 'get').
+        var apiClientSpy = spyOn(this.apiClient, 'get').
           andCallFake(function(path, responseBodyCallback) {
             if (path.indexOf("results")>=0) {
               responseBodyCallback('[{"a":"101","b":"102"}, {"a":"201","b":"202"}]')
@@ -67,7 +67,7 @@ describe('splunk search job', function(){
             
           })
     
-        var job = new SplunkSearchJob(this.splunkHttp, {search:"foo"})
+        var job = new SplunkSearchJob(this.apiClient, {search:"foo"})
         job._jobId = "1234.567"    
         job.fetchJsonResultsForJob(function(results){
           expect(results).toEqual([{a:"101",b:"102"}, {a:"201",b:"202"}])
@@ -84,44 +84,44 @@ describe('splunk search job', function(){
   
   describe('checking whether we have all the results for an existing search job', function(){
     it('requests job status', function(){
-      var splunkHttpSpy = spyOn(this.splunkHttp, 'get').andCallFake(function() {})
-      var job = new SplunkSearchJob(this.splunkHttp, {search:"foo"})
+      var apiClientSpy = spyOn(this.apiClient, 'get').andCallFake(function() {})
+      var job = new SplunkSearchJob(this.apiClient, {search:"foo"})
       job._jobId = "1234.567"    
       job.checkWhetherWeHaveAllResults(7, function(){})
       
-      expect(splunkHttpSpy.mostRecentCall.args[0]).toEqual('/services/search/jobs/1234.567')
+      expect(apiClientSpy.mostRecentCall.args[0]).toEqual('/services/search/jobs/1234.567')
     })
 
     it('is not finished if isDone != 1, even if the offset is > the result count', function(){
-      var splunkHttpSpy = 
-        spyOn(this.splunkHttp, 'get').
+      var apiClientSpy = 
+        spyOn(this.apiClient, 'get').
           andCallFake(function(url, responseBodyCallback) {
             responseBodyCallback("<foo>\n<s:key name=\"isDone\">0</s:key>\n<s:key name=\"resultCount\">5</s:key>\n</foo>")
           })
-      var job = new SplunkSearchJob(this.splunkHttp, {search:"foo"})
+      var job = new SplunkSearchJob(this.apiClient, {search:"foo"})
       job._jobId = "1234.567"    
       job.checkWhetherWeHaveAllResults(7, 
         function(weHaveAllResults) { expect(weHaveAllResults).toEqual(false) })
     })
 
     it('is not finished if isDone = 1, but the offset is < the result count', function(){
-      var splunkHttpSpy = 
-        spyOn(this.splunkHttp, 'get').
+      var apiClientSpy = 
+        spyOn(this.apiClient, 'get').
           andCallFake(function(url, responseBodyCallback) {
             responseBodyCallback("<foo>\n<s:key name=\"isDone\">1</s:key>\n<s:key name=\"resultCount\">9</s:key>\n</foo>")
           })
-      var job = new SplunkSearchJob(this.splunkHttp, {})  
+      var job = new SplunkSearchJob(this.apiClient, {})  
       job.checkWhetherWeHaveAllResults(7, 
         function(weHaveAllResults) { expect(weHaveAllResults).toEqual(false) })
     })
 
     it('is finished if isDone = 1 and the offset is = the result count', function(){
-      var splunkHttpSpy = 
-        spyOn(this.splunkHttp, 'get').
+      var apiClientSpy = 
+        spyOn(this.apiClient, 'get').
           andCallFake(function(url, responseBodyCallback) {
             responseBodyCallback("<foo>\n<s:key name=\"isDone\">1</s:key>\n<s:key name=\"resultCount\">7</s:key>\n</foo>")
           })
-      var job = new SplunkSearchJob(this.splunkHttp, {})
+      var job = new SplunkSearchJob(this.apiClient, {})
       job.checkWhetherWeHaveAllResults(7, 
         function(weHaveAllResults) { expect(weHaveAllResults).toEqual(true) })
     })
