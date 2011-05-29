fc3e0ec466055c5611150cdba24f65418664508e
Steve Conover <steve@squareup.com>
2011-05-29 08:11:17 -0700

    send results to the client as they come in

--- spec/server/socket_spec.js
+++ spec/server/socket_spec.js
@@ -9,6 +9,12 @@ describe('splunk server socket', function(){
     }
     this.message = function(message) { eventHandlers.message(message) }
     this.disconnect = function() { eventHandlers.disconnect() }
+    
+    this.sent = []
+    var self = this
+    this.send = function(jsonStruct) {
+      self.sent.push(jsonStruct)
+    }
   }
   var FakeRawSocket = function(){
     var eventHandlers = {}
@@ -18,7 +24,7 @@ describe('splunk server socket', function(){
     this.connection = function(client) { eventHandlers.connection(client) }
   }
   
-  it('executes a splunk search based on the message received from the client', function(){
+  it('executes a splunk search based on the json-serialized search received from the client', function(){
     var fakeRawSocket = new FakeRawSocket()
     var splunkSocket = 
       new SplunkServerSocket({
@@ -45,7 +51,39 @@ describe('splunk server socket', function(){
       search: "source=cars | head 100"
     })
   })
+  
+  it('sends results of the search to the client as they come in', function(){
+    var fakeRawSocket = new FakeRawSocket()
+    var splunkSocket = 
+      new SplunkServerSocket({
+        user: 'admin',
+        password: 'pass',
+        host: 'splunk.example.com',
+        port: 8089
+      }, fakeRawSocket)
+    
+    var splunkSearchSpy = spyOn(splunkSocket, 'runSplunkSearch').andCallFake(function() {return {}})
+    
+    var fakeClient = new FakeClient()
+    fakeRawSocket.connection(fakeClient)
+    
+    fakeClient.message(JSON.stringify({search: "source=cars | head 100"}))
+    
+    var onNextResultsCallback = splunkSearchSpy.mostRecentCall.args[1]
+    onNextResultsCallback([{color:'red'},{color:'blue'}])
+    
+    expect(fakeClient.sent).toEqual([
+      [{color:'red'},{color:'blue'}]
+    ])
+    
+    onNextResultsCallback([{color:'green'}])
 
+    expect(fakeClient.sent).toEqual([
+      [{color:'red'},{color:'blue'}],
+      [{color:'green'}]
+    ])
+
+  })
 })
 
 
